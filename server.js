9512121bf9e560afa52a5c95ecf65ad517cd72d9
Mohammad Ali <itsproali@gmail.com>
2022-05-15 10:02:23 +0600

    Services Added

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.jggf1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("DoctorsPortal").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    //   client.close()
+  }
+};
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Doctors Server is Running");
+});
+
+app.listen(port, () => {
+  console.log("Server Running on: ", port);
+});
