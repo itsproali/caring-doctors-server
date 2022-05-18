36dff903c6cb7e2a46e64cab83819ae42a9b9264
Mohammad Ali <itsproali@gmail.com>
2022-05-18 12:21:22 +0600

    Jwt Verification Added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+var jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,6 +9,21 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.jggf1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -21,6 +37,7 @@ const run = async () => {
     await client.connect();
     const serviceCollection = client.db("DoctorsPortal").collection("services");
     const bookingCollection = client.db("DoctorsPortal").collection("bookings");
+    const userCollection = client.db("DoctorsPortal").collection("users");
 
     // Get All Services
     app.get("/services", async (req, res) => {
@@ -66,11 +83,34 @@ const run = async () => {
     });
 
     // My Appointment
-    app.get("/myappointment", async (req, res) => {
+    app.get("/myappointment", verifyJwt, async (req, res) => {
+      const decodeId = req.decoded.userId;
       const patientId = req.query.patientId;
-      const query = { patientId };
-      const result = await bookingCollection.find(query).toArray();
-      res.send(result);
+      if (decodeId === patientId) {
+        const query = { patientId };
+        const result = await bookingCollection.find(query).toArray();
+        return res.send(result);
+      } else {
+        return res.status(403).send({ message: "Forbidden Access" });
+      }
+    });
+
+    // Create User
+    app.put("/user/:uid", async (req, res) => {
+      const user = req.body;
+      const uid = req.params.uid;
+      const query = { uid };
+      const options = { upsert: true };
+      const updateInfo = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(query, updateInfo, options);
+      const token = jwt.sign(
+        { userId: user.user.uid },
+        process.env.SECRET_TOKEN,
+        { expiresIn: "12h" }
+      );
+      res.send({ result, token });
     });
   } finally {
     //   client.close()
